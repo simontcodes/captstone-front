c862863591531c97880e581507b2b7941f94a9e6
simon tang <simont.codes@gmail.com>
2022-09-18 00:11:32 -0400

    add google maps iframe to show location of company

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,11 +1,19 @@
-
-
-function Contact () {
-    return(
-        <>
-        <h1>This is the contact page</h1>
-        </>
-    );
+function Contact() {
+  return (
+    <>
+      <h1>This is the contact page</h1>
+      <iframe
+        title="office address"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2039.7194453248658!2d-79.3982346160376!3d43.69810141350756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b3338af13ccbf%3A0x277026dd12f167e2!2sTim%20Hortons!5e0!3m2!1sen!2sca!4v1663473132883!5m2!1sen!2sca"
+        width="600"
+        height="450"
+        // style="border:0;"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </>
+  );
 }
 
 export default Contact;
