9735bae89f28ed6b2a6544d4cd44f4b9b2652bb0
simon tang <simont.codes@gmail.com>
2022-09-26 00:43:18 -0400

    styled log in page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,36 @@
 import "./Login.scss";
+import { Link} from "react-router-dom";
 
-function Login({isLoginError, errorMessage, handleLogin}) {
+function Login({ isLoginError, errorMessage, handleLogin }) {
   return (
-    <div>
-      <h1>Login</h1>
-      {isLoginError && <label style={{ color: "red" }}>{errorMessage}</label>}
-      <form onSubmit={handleLogin}>
-        <div className="form-group">
-         Email: <input type="text" name="email" />
-        </div>
-        <div className="form-group">
-          Password: <input type="password" name="password" />
-        </div>
-        <button className="btn btn-primary" type="submit">
-          Login
-        </button>
-      </form>
+    <div className="login">
+      <div className="login__left">
+        
+      </div>
+      <div className="login__right">
+        {isLoginError && <label style={{ color: "red" }}>{errorMessage}</label>}
+        <form className="login__form" onSubmit={handleLogin}>
+        <h1>Login</h1>
+          <div >
+          <input placeholder="Email" className="login__inputs" type="text" name="email" />
+          </div>
+          <div >
+            <input placeholder="Password"  className="login__inputs" type="password" name="password" />
+          </div>
+          <Link className="login__forgot" to={"#"}>forgot your password?</Link>
+          <div className="login__buttons">
+          <button  className="login__register">
+            Register
+          </button>
+
+          <button className="login__login" type="submit">
+            Sign In
+          </button>
+
+          </div>
+          
+        </form>
+      </div>
     </div>
   );
 }
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,28 +3,26 @@ import { useNavigate } from "react-router-dom";
 
 //use this function to redirect from other pages
 
-function LogInPage({
-  handleLogin,
-  isLoggedIn,
-  isLoginError,
-  errorMessage,
-}) {
+function LogInPage({ handleLogin, isLoggedIn, isLoginError, errorMessage }) {
   const navigate = useNavigate();
-  
-  if(isLoggedIn){
+
+  if (isLoggedIn) {
     setTimeout(() => {
       navigate("/clients");
-    }, 1000)
-}
-
+    }, 1000);
+  }
 
   if (!isLoggedIn)
     return (
-      <Login
-        isLoginError={isLoginError}
-        errorMessage={errorMessage}
-        handleLogin={handleLogin}
-      />
+      <main className="background">
+        <div className="background__card">
+          <Login
+            isLoginError={isLoginError}
+            errorMessage={errorMessage}
+            handleLogin={handleLogin}
+          />
+        </div>
+      </main>
     );
   return (
     <>
