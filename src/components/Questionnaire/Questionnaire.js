4cbab15ed6e87ae8dc050476f18a5d09c8a51ba1
simon tang <simont.codes@gmail.com>
2022-09-26 23:39:09 -0400

    added styling to form step 1 to 3

--- src/components/Questionnaire/Questionnaire.js
+++ src/components/Questionnaire/Questionnaire.js
@@ -1,6 +1,7 @@
 import "./Questionnaire.scss";
 import NextIcon from "../../assets/icons/next.svg";
 import PreviousIcon from "../../assets/icons/previous.svg";
+import PlusSquare from "../../assets/icons/plus-square.svg"
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -178,11 +179,22 @@ function Questionnaire() {
     event.preventDefault();
     setFormItem(formItem + 1);
   }
+
+  const formTitles = [
+    "Please Fill out the form:",
+    "What is your level of education:",
+    "Work experience in the last 10 years:",
+    "Have you been In Canada before?",
+    "Have you taken an official english test?",
+    "Partner information:",
+    "Province or city of preference:",
+    "Review all information before submitting",
+  ];
   //----------------------------------
   return (
     <section className="questions">
       <div className="questions__container">
-        <p>Step{formItem} out of 8</p>
+        <h1 className="questions__title">{formTitles[formItem - 1]}</h1>
         <form
           onSubmit={handleSubmit(submitForm)}
           action="submit"
@@ -190,63 +202,75 @@ function Questionnaire() {
         >
           {formItem === 1 && (
             <>
-              <label htmlFor="firstName">
-                First Name
-                <input
-                  type="text"
-                  name="firstName"
-                  className="form__first-name"
-                  placeholder="First Name"
-                  {...register("firstName")}
-                />
-              </label>
-              <p> {errors.firstName?.message} </p>
-              <label htmlFor="last-name">
-                Last Name
-                <input
-                  type="text"
-                  name="lastName"
-                  className="form__last-name"
-                  placeholder="Last Name"
-                  {...register("lastName")}
-                />
-              </label>
-              <p> {errors.lastName?.message} </p>
-              <label htmlFor="email">
+              <div className="form__two-inputs">
+                <label
+                  className="form__label form__full-name"
+                  htmlFor="firstName"
+                >
+                  First Name
+                  <input
+                    type="text"
+                    name="firstName"
+                    className="form__first-name form__input-text"
+                    placeholder="First Name"
+                    {...register("firstName")}
+                  />
+                  <span> {errors.firstName?.message} </span>
+                </label>
+
+                <label
+                  className="form__label form__full-name"
+                  htmlFor="last-name"
+                >
+                  Last Name
+                  <input
+                    type="text"
+                    name="lastName"
+                    className="form__last-name form__input-text"
+                    placeholder="Last Name"
+                    {...register("lastName")}
+                  />
+                  <span className="form__error">
+                    {" "}
+                    {errors.lastName?.message}{" "}
+                  </span>
+                </label>
+              </div>
+
+              <label className="form__label" htmlFor="email">
                 Email
                 <input
                   type="email"
                   name="email"
-                  className="form__email"
+                  className="form__email form__input-text"
                   placeholder="Email"
                   {...register("email")}
                 />
+                <span className="form__error"> {errors.email?.message} </span>
               </label>
-              <p> {errors.email?.message} </p>
-              <label htmlFor="phone-number">
+
+              <label className="form__label" htmlFor="phone-number">
                 Phone Number
                 <input
                   type="tel"
                   name="phoneNumber"
-                  className="form__phone"
+                  className="form__phone form__input-text"
                   placeholder="Phone Number"
                   {...register("phoneNumber")}
                 />
+                <span className="form__error">
+                  {" "}
+                  {errors.phoneNumber?.message}{" "}
+                </span>
               </label>
-              <p> {errors.phoneNumber?.message} </p>
             </>
           )}
           {formItem === 2 && (
             <>
-              <h2 className="form__title">What is your level of education?</h2>
-
-              <label htmlFor="education-level">
-                Enter the highest level of education for which you:
                 <select
+                  className="form__input-text form__education"
                   {...register("educationLevel")}
                   name="educationLevel"
-                  id="cars"
-                  form="carform"
                 >
                   <option value="None, or less than secondary(high school)">
                     None, or less than secondary(high school)
@@ -256,21 +280,21 @@ function Questionnaire() {
                   </option>
                   <option
                     value="One-year program at a university, college, trade or technical
-            school, or another institute"
+          school, or another institute"
                   >
                     One-year program at a university, college, trade or
                     technical school, or another institute
                   </option>
                   <option
                     value="Two-year program at a university, college, trade or technical
-            school, or another institute"
+          school, or another institute"
                   >
                     Two-year program at a university, college, trade or
                     technical school, or another institute
                   </option>
                   <option
                     value="Bachelor's degree(three or more year program at a university,
-            college, trade or technical school, or another institute)"
+          college, trade or technical school, or another institute)"
                   >
                     Bachelor's degree(three or more year program at a
                     university, college, trade or technical school, or another
@@ -278,21 +302,21 @@ function Questionnaire() {
                   </option>
                   <option
                     value="Two or more certificates, diplomas or degrees. One must be for a
-            program of three or more years"
+          program of three or more years"
                   >
                     Two or more certificates, diplomas or degrees. One must be
                     for a program of three or more years
                   </option>
                   <option
                     value="Two-year program at a university, college, trade or technical
-            school, or another institute"
+          school, or another institute"
                   >
                     Two-year program at a university, college, trade or
                     technical school, or another institute
                   </option>
                   <option
                     value="Master's degree, or professional degree needed to practice in a
-            licensed profession"
+          licensed profession"
                   >
                     Master's degree, or professional degree needed to practice
                     in a licensed profession
@@ -301,31 +325,30 @@ function Questionnaire() {
                     Doctoral level university degree(PhD)
                   </option>
                 </select>
-              </label>
-              <p> {errors.educationLevel?.message} </p>
+                <span> {errors.educationLevel?.message} </span>
+             
             </>
           )}
           {formItem === 3 && (
             <>
-              <h1>
-                In the last ten years, how many years of work experience do you
-                have? Main Job Title & duration
-              </h1>
+            <div className="form__work-exp">
               {workExp.map((jobExp) => {
                 return (
-                  <div key={jobExp}>
-                    <label htmlFor="job">
+                  <div className="form__two-inputs form__work-inputs" key={jobExp}>
+                    <label className="form__full-name" htmlFor="job">
                       Job title:
                       <input
+                      className="form__input-text"
                         {...register(`job${jobExp + 1}`)}
                         type="text"
                         name={`job${jobExp + 1}`}
                         placeholder="enter occupation"
                       />
                     </label>
-                    <label htmlFor="yearsOfExp">
+                    <label className="form__full-name" htmlFor="yearsOfExp">
                       Years of experience:
                       <input
+                       className="form__input-text"
                         {...register(`yearsOfExp${jobExp + 1}`)}
                         type="number"
                         name={`yearsOfExp${jobExp + 1}`}
@@ -334,14 +357,14 @@ function Questionnaire() {
                   </div>
                 );
               })}
-              <button onClick={handleWorkExp}> Add another job </button>
+             <h4 onClick={handleWorkExp} className="form__work-title"><img  src={PlusSquare} alt="Plus Icon" />  add another work experience</h4> 
+              </div>
             </>
           )}
           {formItem === 4 && (
             <>
               {!beenCanada && (
                 <>
-                  <h1>Have you been in Canada before? </h1>
                   <button onClick={hasBeen}>Yes</button>
                   <button onClick={hasNotBeen}>No</button>
                 </>
@@ -455,7 +478,6 @@ function Questionnaire() {
           )}
           {formItem === 5 && (
             <>
-              <h2>Have you done an official English IELTS or Celpip test?</h2>
               <button
                 onClick={(event) => {
                   handleHasEnglish(event);
@@ -544,7 +566,6 @@ function Questionnaire() {
           )} */}
           {formItem === 7 && (
             <>
-              <h2>Do you have a province or city of preference?</h2>
               <label htmlFor="province">
                 Select a province or Territory:
                 <select
@@ -590,8 +611,12 @@ function Questionnaire() {
 
               <h2>Are you open to study in Canada?</h2>
               <label htmlFor="study">
-                <select defaultValue={""} {...register("studyInCanada")} name="studyInCanada">
-                <option disabled ></option>
+                <select
+                  defaultValue={""}
+                  {...register("studyInCanada")}
+                  name="studyInCanada"
+                >
+                  <option disabled></option>
                   <option value="yes">yes</option>
                   <option value="no">No</option>
                 </select>
@@ -601,9 +626,6 @@ function Questionnaire() {
           )}
           {formItem === 8 && (
             <>
-              <h2>
-                Please confirm all information is correct before submitting
-              </h2>
               <p>First Name: {watch("firstName")}</p>
               <p>Last Name: {watch("lastName")}</p>
               <p>Email: {watch("email")}</p>
@@ -652,55 +674,102 @@ function Questionnaire() {
             </>
           )}
         </form>
-
+      </div>
+      <div className="form__navigation-arrows">
         {formItem !== 1 && (
-          <button onClick={previousQuestion}>
-            <img src={PreviousIcon} alt="" />
-          </button>
+          <img
+            className="form__arrows"
+            onClick={previousQuestion}
+            src={PreviousIcon}
+            alt=""
+          />
         )}
         {formItem !== 8 && (
-          <button onClick={nextQuestion}>
-            <img src={NextIcon} alt="" />
-          </button>
+          <img
+            className="form__arrows"
+            onClick={nextQuestion}
+            src={NextIcon}
+            alt=""
+          />
         )}
       </div>
       <div className="progress">
-        <div onClick={() =>{setFormItem(1)}} className={`circle ${ (formItem >= 1) ? "done" : ""}`}>
+        <div
+          onClick={() => {
+            setFormItem(1);
+          }}
+          className={`circle ${formItem >= 1 ? "done" : ""}`}
+        >
           <span className="label">1</span>
           <span className="title">Personal</span>
         </div>
         <span className="bar done"></span>
-        <div onClick={() =>{setFormItem(2)}} className={`circle ${ (formItem >= 2) ? "done" : ""}`}>
+        <div
+          onClick={() => {
+            setFormItem(2);
+          }}
+          className={`circle ${formItem >= 2 ? "done" : ""}`}
+        >
           <span className="label">2</span>
           <span className="title">Education</span>
         </div>
         <span className="bar half"></span>
-        <div onClick={() =>{setFormItem(3)}} className={`circle ${ (formItem >= 3) ? "done" : ""}`}>
+        <div
+          onClick={() => {
+            setFormItem(3);
+          }}
+          className={`circle ${formItem >= 3 ? "done" : ""}`}
+        >
           <span className="label">3</span>
           <span className="title">Work</span>
         </div>
         <span className="bar"></span>
-        <div onClick={() =>{setFormItem(4)}} className={`circle ${ (formItem >= 4) ? "done" : ""}`}>
+        <div
+          onClick={() => {
+            setFormItem(4);
+          }}
+          className={`circle ${formItem >= 4 ? "done" : ""}`}
+        >
           <span className="label">4</span>
           <span className="title">Canada</span>
         </div>
         <span className="bar"></span>
-        <div onClick={() =>{setFormItem(5)}} className={`circle ${ (formItem >= 5) ? "done" : ""}`}>
+        <div
+          onClick={() => {
+            setFormItem(5);
+          }}
+          className={`circle ${formItem >= 5 ? "done" : ""}`}
+        >
           <span className="label">5</span>
           <span className="title">English</span>
         </div>
         <span className="bar"></span>
-        <div onClick={() =>{setFormItem(6)}} className={`circle ${ (formItem >= 6) ? "done" : ""}`}>
+        <div
+          onClick={() => {
+            setFormItem(6);
+          }}
+          className={`circle ${formItem >= 6 ? "done" : ""}`}
+        >
           <span className="label">6</span>
           <span className="title">Partner</span>
         </div>
         <span className="bar"></span>
-        <div onClick={() =>{setFormItem(7)}} className={`circle ${ (formItem >= 7) ? "done" : ""}`}>
+        <div
+          onClick={() => {
+            setFormItem(7);
+          }}
+          className={`circle ${formItem >= 7 ? "done" : ""}`}
+        >
           <span className="label">7</span>
           <span className="title">Preference</span>
         </div>
         <span className="bar"></span>
-        <div onClick={() =>{setFormItem(8)}} className={`circle ${ (formItem >= 8) ? "done" : ""}`}>
+        <div
+          onClick={() => {
+            setFormItem(8);
+          }}
+          className={`circle ${formItem >= 8 ? "done" : ""}`}
+        >
           <span className="label">8</span>
           <span className="title">Review</span>
         </div>
